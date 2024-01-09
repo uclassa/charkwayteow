720b92c227f0ed121741a210f38bac2c57e925a5
Alex Su <suziyue@gmail.com>
2024-01-09 15:59:16 -0800

    Removed pycache files, updated gitignore and database connection

---
+++ ssabackend/settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE charkwayteow;
+CREATE USER cktadmin WITH PASSWORD 'i12eatckt';
+GRANT ALL PRIVILEGES ON DATABASE charkwayteow to cktadmin;
--- ssabackend/ssabackend/settings.py
+++ ssabackend/ssabackend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ssabackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'charkwayteow', 
+        'USER': 'cktadmin',
+        'PASSWORD': 'i12eatckt',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
