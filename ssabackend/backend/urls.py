5ed26739700a75233155b370021a23d5a4263065
Alex Su <suziyue@gmail.com>
2024-01-25 01:04:52 -0800

    Updated widgets on admin portal, made event participants invisible to non-admins

--- ssabackend/backend/urls.py
+++ ssabackend/backend/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 router = routers.SimpleRouter()
 router.register(r"events", views.EventViewSet)
-router.register(r"family", views.FamilyViewSet)
-router.register(r"member", views.MemberViewSet)
+router.register(r"families", views.FamilyViewSet)
+router.register(r"members", views.MemberViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- ssabackend/backend/views.py
+++ ssabackend/backend/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, permissions
-from backend.serializers import EventSerializer, FamilySerializer, MemberSerializer
+from backend.serializers import EventSerializer, EventPublicSerializer, FamilySerializer, MemberSerializer
 from backend.models import Event, Family, Member
 
 # Create your views here.
@@ -8,21 +8,37 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         return request.user.is_staff
 
+
+# Event is read-only for non-admins
+# Participants invisible to non-admins
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
-    serializer_class = EventSerializer
     permission_classes = [IsAdminOrReadOnly]
 
+    def get_serializer_class(self):
+        return EventSerializer if self.request.user.is_staff else EventPublicSerializer
 
+
+# Only admins can view families and list members
 class FamilyViewSet(viewsets.ModelViewSet):
     queryset = Family.objects.all()
     serializer_class = FamilySerializer
     permission_classes = [permissions.IsAdminUser]
 
+
+# User has only permission for their own member object
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     permission_classes = [permissions.IsAdminUser]
+
+
+"""
+TODO: 
+- Endpoints for user rsvp to event (add/remove me from event)
+- me endpoint (get user and related profile info, update profile info)
+- creating user and associate with member on first login
+"""
