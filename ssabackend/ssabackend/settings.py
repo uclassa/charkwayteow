fe3da3664841dce18752e917cf606a3f4dc963ff
Alex Su <suziyue@gmail.com>
2024-01-26 02:03:02 -0800

    trusted origins

--- ssabackend/ssabackend/settings.py
+++ ssabackend/ssabackend/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = ['charkwayteow-development.up.railway.app']
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
