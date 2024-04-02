aa33155caef049845454d7cede19a8ef4f2dddcf
Alex Su <suziyue@gmail.com>
2024-04-01 23:21:29 -0700

    secret admin site url

--- ssabackend/ssabackend/urls.py
+++ ssabackend/ssabackend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from environ import Env
+
+env = Env(
+    DJANGO_ADMIN_URL=(str, 'admin/'),
+)
 
 urlpatterns = [
     path('', include("backend.urls")),
-    path('admin/', admin.site.urls),
+    path(env('DJANGO_ADMIN_URL'), admin.site.urls),
 ]
