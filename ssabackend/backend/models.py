bc16aa59cab7940b22a61cc6fd608de99a980dab
Alex Su <suziyue@gmail.com>
2024-04-04 13:25:33 -0700

    fixed import error for drive module

--- ssabackend/backend/models.py
+++ ssabackend/backend/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from drive import ModifiedGoogleDriveStorage
+from .drive import ModifiedGoogleDriveStorage
 
 gd_storage = ModifiedGoogleDriveStorage()
 
