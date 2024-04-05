73b7479e4f80675f5b82fa583a14265a21473ca0
Alex Su <suziyue@gmail.com>
2024-04-04 21:08:26 -0700

    workaround for slow fetching of data involving images. overridden model save method, added new cached image url field to model, changed public serializer to map to cached field

--- ssabackend/backend/models.py
+++ ssabackend/backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from .drive import ModifiedGoogleDriveStorage
 
-gd_storage = ModifiedGoogleDriveStorage()
+gd_storage = ModifiedGoogleDriveStorage()    
 
 # Create your models here.
 class Event(models.Model):
@@ -12,8 +12,35 @@ class Event(models.Model):
     participants = models.ManyToManyField(to="Member", related_name="events", blank=True)
     venue = models.TextField()
     description = models.TextField(blank=True)
+    image_url = models.URLField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True, upload_to="event_images", storage=gd_storage)
     link = models.URLField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Check if the instance is being updated
+        if self.pk:
+            try:
+                # Retrieve the previous value of the image field
+                prev_instance = Event.objects.get(pk=self.pk)
+                prev_image = prev_instance.image
+            except Event.DoesNotExist:
+                prev_image = None
+
+        # Call the parent class's save method
+        super().save(*args, **kwargs)
+
+        # Check if the image field has been updated or cleared
+        if self.image != prev_image:
+            if self.image:
+                # Generate the URL of the image based on its path
+                # Update the image_url field with the URL
+                self.image_url = self.image.url
+            else:
+                # If the image field has been cleared, clear the image_url field as well
+                self.image_url = None
+            # Save the model again to update the image_url field
+            super().save(update_fields=['image_url'])
+
 
     def __str__(self):
         return self.title
@@ -25,8 +52,34 @@ class Member(models.Model):
     phone = models.CharField(max_length=15, blank=True)
     gender = models.CharField(max_length=10, blank=True)
     family = models.ForeignKey(to="Family", on_delete=models.SET_NULL, null=True, blank=True, related_name="members")
+    profile_image_url = models.URLField(blank=True, null=True)
     profile_image = models.ImageField(blank=True, null=True, upload_to="profile_images", storage=gd_storage)
     user = models.OneToOneField(to=User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Check if the instance is being updated
+        if self.pk:
+            try:
+                # Retrieve the previous value of the image field
+                prev_instance = Member.objects.get(pk=self.pk)
+                prev_image = prev_instance.image
+            except Member.DoesNotExist:
+                prev_image = None
+
+        # Call the parent class's save method
+        super().save(*args, **kwargs)
+
+        # Check if the image field has been updated or cleared
+        if self.image != prev_image:
+            if self.image:
+                # Generate the URL of the image based on its path
+                # Update the image_url field with the URL
+                self.profile_image_url = self.image.url
+            else:
+                # If the image field has been cleared, clear the image_url field as well
+                self.image_url = None
+            # Save the model again to update the image_url field
+            super().save(update_fields=['profile_image_url'])
 
     def __str__(self):
         return self.name
--- ssabackend/backend/serializers.py
+++ ssabackend/backend/serializers.py
@@ -12,12 +12,18 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+        read_only_fields = ('image_url',)
 
 
 class EventPublicSerializer(serializers.ModelSerializer):
+    # This is the read only serializer for the website and telebot
+    # The image field pulls from the cached url on the model so google drive storage is not called
+    # Otherwise api calls will be very very slowwwwwwww
+    image = serializers.URLField(source='image_url')
+
     class Meta:
         model = Event
-        exclude = ('id', 'participants')
+        fields = ('title', 'start_date', 'end_date', 'venue', 'description', 'image', 'link')
 
 
 class FamilySerializer(serializers.ModelSerializer):
@@ -47,3 +53,9 @@ class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = '__all__'
+        read_only_fields = ('profile_image_url',)
+
+
+'''
+TODO: Since the only editable data by users is their profile, only the member serializer needs to be modified to output the image url but take in an image. Cross that bridge when we get there.
+'''
