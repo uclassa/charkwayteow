925bd997beb6df813b66a0144f8436e1fb009e5c
Alex Su <suziyue@gmail.com>
2024-06-29 04:13:57 +0800

    Added drive widget to manage service account

---
+++ ssabackend/drive.py
@@ -0,0 +1,20 @@
+import os, json
+from gdstorage.storage import GoogleDriveStorage
+os.environ["DJANGO_SETTINGS_MODULE"] = "ssabackend.settings"
+
+def main():
+    s = GoogleDriveStorage()._drive_service
+    while line := input():
+        arr = line.split()
+        if arr[0] == "list":
+            try:
+                print(json.dumps(s.files().list(q=f"'{arr[1]}' in parents").execute(), indent=4))
+            except IndexError:
+                print(json.dumps(s.files().list().execute(), indent=4))
+        elif arr[0] == "delete":
+            for item in arr[1:]:
+                s.files().delete(fileId=item).execute()
+            print(json.dumps(s.files().list().execute(), indent=4))
+
+if __name__ == "__main__":
+    main()
