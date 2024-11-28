064906c954eb93f40e19a0e46dd75f2dbacbd04d
Alex Su <suziyue@gmail.com>
2024-11-28 01:37:36 -0800

    Changed to class based approach and implemented path comprehension in rm command

--- drive.py
+++ drive.py
@@ -1,7 +1,7 @@
 import os, shlex
 from copy import copy
 from googleapiclient.errors import HttpError
-from gdstorage.storage import GoogleDriveStorage
+from gdstorage.storage import GoogleDriveStorage as gds
 os.environ["DJANGO_SETTINGS_MODULE"] = "charkwayteow.settings"
 
 """
@@ -21,76 +21,120 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
+class DriveManager:
+    def __init__(self):
+        self.s = gds()._drive_service
+        self.context = []
+    
+    def curr_folder_id(self, context = None):
+        if context is None:
+            context = self.context
+        return context[-1][1] if context else "root"
+    
+    def curr_path(self):
+        return "/" + "/".join([name for name, _ in self.context])
+    
+    def is_directory(self, file):
+        return file.get("mimeType") == gds._GOOGLE_DRIVE_FOLDER_MIMETYPE_
+    
+    def ls(self):
+        res = self.s.files().list(q=f"'{self.curr_folder_id()}' in parents").execute()
+        for file in res["files"]:
+            name = file["name"]
+            if self.is_directory(file):
+                name += "/"
+            print(name)
+
+    def cd(self, file, context = None):
+        if not file:
+            return
+        if context is None:
+            context = self.context
+        file = file.rstrip("/")
+        if file.startswith("/"):
+            new_context = []
+            file = file.lstrip("/")
+        else:
+            new_context = copy(self.context)
+        error = False
+        if not file:
+            return
+        for name in file.split("/"):
+            match name:
+                case "..":
+                    if not new_context:
+                        error = True
+                        break
+                    new_context.pop()
+                case ".":
+                    pass
+                case _:
+                    res = self.s.files().list(q=f"name = '{name}' and mimeType = '{gds._GOOGLE_DRIVE_FOLDER_MIMETYPE_}' and '{self.curr_folder_id(new_context)}' in parents").execute()
+                    if len(res["files"]) == 0:
+                        error = True
+                        break
+                    new_context.append((name, res["files"][0]["id"]))
+        if error:
+            print(f"No such directory: {name}")
+        else:
+            context.clear()
+            context.extend(new_context)
+        return not error
+
+    def rm(self, files):
+        for file in files:
+            split_name = file.rstrip("/").rsplit("/", 1)
+            match len(split_name):
+                case 1:
+                    dirname, filename = ".", split_name[-1]
+                case 2:
+                    dirname, filename = split_name
+                case _:
+                    return
+            context = copy(self.context)
+            if not self.cd(dirname, context):
+                return
+            res = self.s.files().list(q=f"name = '{filename}' and '{self.curr_folder_id(context)}' in parents").execute()
+            if len(res["files"]) == 0:
+                print(f"No such file: {filename}")
+                return
+            file_data = res["files"][0]
+            if self.is_directory(file_data):
+                while True:
+                    decision = input(f"{filename} is a directory. Confirm delete? y/N: ")
+                    match decision:
+                        case "Y" | "y" | "yes":
+                            remove = True
+                            break
+                        case "N" | "n" | "no" | "":
+                            remove = False
+                            break
+            else:
+                remove = True
+            if remove:
+                self.s.files().delete(fileId=file_data["id"]).execute()
+
+
 def main():
-    s = GoogleDriveStorage()._drive_service
+    dm = DriveManager()
     print("Available commands: ls, cd, rm <file> <file> ..")
-    print("Note: rm only supports files and directories in the current folder because Alex is lazy and hasn't implemented slash support yet")
-    context = []
     while True:
         try:
-            line = input(f"{bcolors.OKBLUE}/{"/".join(map(lambda tup: tup[0], context))}{bcolors.OKGREEN}$ {bcolors.ENDC}")
+            line = input(f"{bcolors.OKBLUE}{dm.curr_path()}{bcolors.ENDC}$ ")
             if not line:
                 continue
             arr = shlex.split(line)
             try:
-                cmd = s.files()
                 match arr[0]:
                     case "ls":
-                        res = cmd.list(q=f"'{context[-1][1] if context else "root"}' in parents").execute()
-                        for file in res["files"]:
-                            name = file["name"]
-                            if file["mimeType"] == "application/vnd.google-apps.folder":
-                                name += "/"
-                            print(name)
+                        dm.ls()
                     case "cd":
                         if len(arr) > 1:
-                            arr[1] = arr[1].rstrip("/")
-                            if arr[1].startswith("/"):
-                                new_context = []
-                                file = arr[1].lstrip("/")
-                            else:
-                                new_context = copy(context)
-                                file = arr[1]
-                            error = False
-                            if file:
-                                for name in file.split("/"):
-                                    match name:
-                                        case "..":
-                                            if not new_context:
-                                                error = True
-                                                break
-                                            new_context.pop()
-                                        case ".":
-                                            pass
-                                        case _:
-                                            res = cmd.list(q=f"name = '{name}' and mimeType = 'application/vnd.google-apps.folder' and '{new_context[-1][1] if new_context else "root"}' in parents").execute()
-                                            if len(res["files"]) == 0:
-                                                error = True
-                                                break
-                                            new_context.append((name, res["files"][0]["id"]))
-                            if error:
-                                print(f"No such directory: {arr[1]}")
-                            else:
-                                context = new_context
+                            dm.cd(arr[1])
                     case "rm":
-                        for item in arr[1:]:
-                            res = cmd.list(q=f"name = '{item.rstrip("/")}' and '{context[-1][1] if context else "root"}' in parents").execute()
-                            if not res["files"]:
-                                print(f"No such file: {item}")
-                                break
-                            file = res["files"][0]
-                            if file["mimeType"] == "application/vnd.google-apps.folder":
-                                while True:
-                                    decision = input(f"{file["name"]} is a directory. Confirm delete? y/N: ")
-                                    match decision:
-                                        case "Y" | "y" | "yes":
-                                            remove = True
-                                            break
-                                        case "N" | "n" | "no" | "":
-                                            remove = False
-                                            break
-                            if remove:
-                                cmd.delete(fileId=file["id"]).execute()
+                        if len(arr) > 1:
+                            dm.rm(arr[1:])
             except HttpError as e:
                 print(e.reason)
         except EOFError:
