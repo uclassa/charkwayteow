a616f8e0e09a008d33fb791d793e37665dc5839c
Alex Su <suziyue@gmail.com>
2025-02-04 11:51:01 -0800

    Removed requirement to set cors_allowed_origins in debug mode

--- charkwayteow/settings.py
+++ charkwayteow/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = ['*']
 
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS').split(',')
+else:
+    CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS').split(',')
 
 
 # Application definition
